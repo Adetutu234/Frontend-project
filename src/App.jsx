a2f521679d9c44613925b008851ca3c2aef502c4
Adetutu234 <apanisilemorenikeji@gmail.com>
2025-07-11 21:40:15 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 
 const App = () => {
+const [users , setUsers]= useState([])
+const [error , setError]= useState(null)
+
+const DOMAIN = "https://backend-project-1-1qj9.onrender.com"
+useEffect(()=>{
+  const getUsers = async () => {
+    try{
+      const res = await fetch(`${DOMAIN}/api/users`)
+      if (!res.ok) throw new Error("failed to get users")
+      const data = await res.json()
+      setUsers (data.users)
+    }catch(error){
+      setError (error.message)
+    }
+}
+ getUsers()
+},[])
+
   return (
     <div>
       <Navbar/>
+      {users.map((user)=>(
+        <li key={user.id}>{user.name}</li>
+      ))}
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex justify-between bg-slate-700 p-5'>
-      <div className='flex gap-5'>
+      <div className='flex gap-5 uppercase'>
         <img src="" alt="" />
         <p>FashionHub</p>
       </div>
 
       <div>
-        <ul className='flex gap-5'>
+        <ul className='flex gap-5 uppercase'>
             <li>home</li>
             <li>about</li>
             <li>services</li>
